56add08f87a2c64d9c131f169c699844c4cbb06a
jonathan-martz <github@jmartz.de>
2025-12-22 16:13:06 +0100

    Add project as submodule under player folder

---
+++ custom/components/WinnerPoints.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="btn text-center btn-block btn-primary">
+    Gewinner:
+    {{ props.data.team?.name }}
+    {{ props.data.score }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  data: {type: Object, required: true},
+});
+</script>
