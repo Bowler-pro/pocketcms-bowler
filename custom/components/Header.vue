db3e5ec88bb4a7a40edf3438ea61ed93a3ccd9d5
jonathan-martz <github@jmartz.de>
2025-12-28 13:15:24 +0100

    fix header spaceing

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-white px-3 py-3">
+  <section class="bg-white px-3 py-3 mb-3 ">
     <a href="/">
       <img src="/bowler-pro-slogan.svg" class="h-12 sm:h-16" alt="">
     </a>
