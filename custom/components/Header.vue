da76f81f16a074a213e29e96331a6349abf5c923
jonathan-martz <github@jmartz.de>
2025-12-28 13:14:17 +0100

    adjust header logo size

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="bg-white px-3 py-3">
-    <img src="/bowler-pro-slogan.svg" class="h-16" alt="">
+    <a href="/">
+      <img src="/bowler-pro-slogan.svg" class="h-12 sm:h-16" alt="">
+    </a>
   </section>
 </template>
 <script setup lang="ts">
